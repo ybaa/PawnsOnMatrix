c9d12c8c0e78799cdb4d284d239b3c090523a6e0
Martyna Łagożna <m.lagozna@live.com>
2017-06-04 14:11:01 +0200

    finished program - this time with correctly working threads

--- PawnsOnMatrix/src/PawnsOnMatrix.cpp
+++ PawnsOnMatrix/src/PawnsOnMatrix.cpp
@@ -6,96 +6,128 @@
 #include <vector>
 #include <random>
 #include <chrono>
-#include <unistd.h>	//sleep
 #include <thread>
 #include <mutex>
 
-
 using namespace std;
 
-vector<int> XCoordinates;
-vector<int> YCoordinates;
 vector<Pawn> pawns;
+mutex mx1;
+mutex mx2;
 
 void addRandomCoordinatesToQueues(vector<int> *Xx, vector<int> *Yy, int numberOfCollumns, int numberOfRows){
-	vector<int> &X = *Xx;
-	vector<int> &Y = *Yy;
-	mt19937 mt;
-	mt.seed(std::chrono::system_clock::now().time_since_epoch().count());
-	uniform_int_distribution<int> *fixedDist;
+	while(true){
+		vector<int> &X = *Xx;
+		vector<int> &Y = *Yy;
 
-	int maxValue = numberOfCollumns-1;
-	fixedDist = new uniform_int_distribution<int>(0, maxValue);
-	int num = (*fixedDist)(mt);
-	X.push_back(num);
+		mt19937 mt;
+		mt.seed(std::chrono::system_clock::now().time_since_epoch().count());
+		uniform_int_distribution<int> *fixedDist;
 
-	maxValue = numberOfRows -1;
-	fixedDist = new uniform_int_distribution<int>(0,maxValue );
-	num = (*fixedDist)(mt);
-	Y.push_back(num);
+		int maxValue = numberOfCollumns-1;
+		fixedDist = new uniform_int_distribution<int>(0, maxValue);
+		int num = (*fixedDist)(mt);
 
-	delete fixedDist;
+		mx1.lock();
+		X.push_back(num);
+
+		maxValue = numberOfRows -1;
+		fixedDist = new uniform_int_distribution<int>(0,maxValue );
+		num = (*fixedDist)(mt);
+
+		Y.push_back(num);
+		mx1.unlock();
+
+		delete fixedDist;
+	}
 }
 
-void DrawPawnAndMoveOthers(vector<int> *Xx, vector<int> *Yy, int numberOfCollumns, int numberOfRows){
-	vector<int> &X = *Xx;
-	vector<int> &Y = *Yy;
+void addPawn(vector<int> *Xx, vector<int> *Yy, int numberOfCollumns, int numberOfRows){
+	while(true){
+		vector<int> &X = *Xx;
+		vector<int> &Y = *Yy;
+		bool canAddPawn = true;
 
-	Pawn p = Pawn();
-	bool canDraw = true;
+		Pawn p = Pawn();
 
-	for(int i = 0; i < pawns.size(); i++){
-		if(pawns[i].getCollumn() == X[0] && pawns[i].getRow() == Y[0])
-			canDraw = false;
-		else
-			canDraw = true;
-	}
-
-	if(canDraw){
-	p.drawPawn(Y[0], X[0]);
-	pawns.push_back(p);
-	X.erase(X.begin());
-	Y.erase(Y.begin());
-	}
-
-	for(int i = 0; i < pawns.size()-1; i++){
-		random_device rd;
-		mt19937 gen(rd());
-		uniform_int_distribution<int> dis(0,3);
-		int direction = dis(gen);
-
-		switch (direction) {
-			case 0:
-				pawns[i].turnLeft(numberOfCollumns-1);
-				break;
-			case 1:
-				pawns[i].turnUp(numberOfRows-1);
-				break;
-			case 2:
-				pawns[i].turnRight(numberOfCollumns-1);
-				break;
-			case 3:
-				pawns[i].turnDown(numberOfRows-1);
-				break;
-			default:
-				break;
+		mx2.lock();
+		for(int i = 0; i < pawns.size(); i++){
+			if(pawns[i].getCollumn() == X[0] && pawns[i].getRow() == Y[0])
+				canAddPawn = false;
+			else
+				canAddPawn = true;
 		}
+
+		mx2.unlock();
+
+		if(canAddPawn){
+			mx2.lock();
+			p.updatePosition(Y[0],X[0]);
+			pawns.push_back(p);
+			mx2.unlock();
+
+			mx1.lock();
+			X.erase(X.begin());
+			Y.erase(Y.begin());
+			mx1.unlock();
+		}
+
+		this_thread::sleep_for(chrono::seconds(1));
 	}
 
-
-
-	refresh();
-	sleep(3);
-	endwin();
 }
+
+void draw(vector<int> *Xx, vector<int> *Yy, int numberOfCollumns, int numberOfRows){
+	while(true){
+		vector<int> &X = *Xx;
+		vector<int> &Y = *Yy;
+
+		mx2.lock();
+		int last = pawns.size()-1;
+
+		pawns[last].drawPawn(pawns[last].getRow(), pawns[last].getCollumn());
+		for(int i = 0; i < pawns.size()-1; i++){
+				random_device rd;
+				mt19937 gen(rd());
+				uniform_int_distribution<int> dis(0,3);
+				int direction = dis(gen);
+
+				switch (direction) {
+					case 0:
+						pawns[i].turnLeft(numberOfCollumns-1);
+						break;
+					case 1:
+						pawns[i].turnUp(numberOfRows-1);
+						break;
+					case 2:
+						pawns[i].turnRight(numberOfCollumns-1);
+						break;
+					case 3:
+						pawns[i].turnDown(numberOfRows-1);
+						break;
+					default:
+						break;
+				}
+			}
+
+		mx2.unlock();
+			refresh();
+			this_thread::sleep_for(chrono::seconds(1));
+			endwin();
+	}
+
+}
+
 
 int main(){
-	mutex mx;
 
+
+	vector<int> XCoordinates;
+	vector<int> YCoordinates;
 	//------------------------------- Create Range -------------------------------------------
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<int> dis(4,16);
+	uniform_int_distribution<int> dis(6,16);
 
 	int numberOfCollumns = dis(gen);
 	int numberOfRows = dis(gen);
@@ -105,23 +137,14 @@ int main(){
 	m.DrawMatrix(numberOfCollumns,numberOfRows);
 
 
+	//------------------------------- Start threads -----------------------------------------
+	thread fillQueuesThread(addRandomCoordinatesToQueues,&XCoordinates,&YCoordinates,numberOfCollumns,numberOfRows);
+	thread addPawnThread(addPawn,&XCoordinates,&YCoordinates,numberOfCollumns,numberOfRows);
+	thread drawThread(draw,&XCoordinates,&YCoordinates,numberOfCollumns,numberOfRows);
 
-	// how to eliminate this for? (important only when I use do while belowe)
-//	for(int i = 0; i < 1; i++){
-//		addRandomCoordinatesToQueues(&XCoordinates,&YCoordinates,numberOfCollumns,numberOfRows);
-//	}
-
-	for(int i = 0; i < 1000 ; i++){
-	//do{
-	thread fillQueues(addRandomCoordinatesToQueues,&XCoordinates,&YCoordinates,numberOfCollumns,numberOfRows);
-
-	mx.lock();
-	fillQueues.join();
-	mx.unlock();
-
-	thread DrawAndMovePawns(DrawPawnAndMoveOthers, &XCoordinates, &YCoordinates, numberOfCollumns, numberOfRows);
-	DrawAndMovePawns.join();
-	}; //while(XCoordinates.empty()==false && YCoordinates.empty() ==false);
+	fillQueuesThread.join();
+	addPawnThread.join();
+	drawThread.join();
 
 	return 0;
 }
